465d532f411e7e224f2ed2b0f251ee1065917a98
Vinayachandran1709 <imvinayachandran@gmail.com>
2026-01-19 00:12:35 +0530

    rem useffect from subscription

--- frontend/src/pages/Dashboard/SubscriptionPage.jsx
+++ frontend/src/pages/Dashboard/SubscriptionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import DashboardHeader from '../../components/dashboard/DashboardHeader';
 import { CreditCard, Calendar, TrendingUp, Mail, Search, Eye, CheckCircle, X } from 'lucide-react';
